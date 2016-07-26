573073591b4d4adbfe60238064659e9ee9aa4a8e
renwenjie1989 <wenjieren@therevolutiongroup.io>
2016-07-26 16:00:44 -0400

    app cannot run

---
+++ src/components/Increment.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<button @click='increment'>Increment + 1</button>
+	</div>
+</template>
+
+<script>
+import { incrementCounter } from '../vuex/action'
+export default {
+	vuex: {
+		action: {
+			increment: incrementCounter
+		}
+	}
+}
+</script>
